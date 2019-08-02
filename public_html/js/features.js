58cc069d892490c314dbd00e584e76a20297b10b
ericgchan <eric@apen4ej.org>
2019-08-02 16:52:36 -0700

    small edits

--- public_html/js/features.js
+++ public_html/js/features.js
@@ -2,23 +2,24 @@
 var tour = new Tour({
     steps: [{
             element: "#top",
-            title: "How to Mass Edit Participant Statuses",
+            title: "How to Mass Edit Participant Statuses for an Event",
             content: 'Click here to access "Events"'
         },
 
         {
             element: "#top2",
-            title: "How to Mass Edit Participant Statuses",
+            title: "How to Mass Edit Participant Statuses for an Event",
             content: 'Click on "Manage Events"',
             path: "/page2.html"
         },
 
         {
             element: "#top3",
-            title: "How to Mass Edit Participant Statuses",
+            title: "How to Mass Edit Participant Statuses for an Event",
             content: 'Search for your event, click on "Participants" and click on the relevant sub-category',
             path: "/page3.html"
         },
+
 
     ],
     debug: true,
@@ -28,7 +29,7 @@ var tour = new Tour({
 });
 
 // Initialize the tour
-tour.init();
+//tour.init();
 
 // Start the tour
 tour.start();
