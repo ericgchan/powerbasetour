5242baf5c7222ee2b36b48e0dfb9f92e4bce4ca3
jose d lopez <jose@devlabs.vc>
2019-08-08 20:20:10 -0500

    COMMENT OUT storage:false, but Check for tour end and restart it - for dev.

--- public_html/js/features.js
+++ public_html/js/features.js
@@ -1,21 +1,23 @@
 // Instance the tour
+// API reference: http://bootstraptour.com/api/
 var tour = new Tour({
     steps: [{
             element: "#top",
-            title: "How to Mass Edit Participant Statuses",
-            content: 'Click here to access "Events"'
+            title: "Part 1:  How to Mass Edit Participant Statuses",
+            content: 'Click here to access "Events"',
+            path: "/index.html"
         },
 
         {
             element: "#top2",
-            title: "How to Mass Edit Participant Statuses",
+            title: "Part 2: How to Mass Edit Participant Statuses",
             content: 'Click on "Manage Events"',
             path: "/page2.html"
         },
 
         {
             element: "#top3",
-            title: "How to Mass Edit Participant Statuses",
+            title: "Part 3: How to Mass Edit Participant Statuses",
             content: 'Search for your event, click on "Participants" and click on the relevant sub-category',
             path: "/page3.html"
         },
@@ -24,7 +26,7 @@ var tour = new Tour({
     debug: true,
     framework: "bootstrap4",
     backdrop: "false",
-    storage: false
+    //storage: false  //THIS WILL RESTART THE TOUR ON EVERY PAGE LOAD (No MEMORY)
 });
 
 // Initialize the tour
@@ -32,3 +34,9 @@ tour.init();
 
 // Start the tour
 tour.start();
+
+// Check if the Tour has ended. Reset it so that future Tours will start
+if (tour.ended() == true) {
+    console.log('tour ended. status variable saved. need to reset it');
+    tour.restart();
+}
