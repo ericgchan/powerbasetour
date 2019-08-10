698c5cf59ab65d0662cd54fdc84607c6685990c6
pujadahal <puja@apen4ej.org>
2019-08-10 12:08:00 -0700

    final tour edits

--- public_html/js/features.js
+++ public_html/js/features.js
@@ -4,29 +4,29 @@ var tour = new Tour({
     steps: [{
         element: "#top",
         title: "Part 1: Create an Event",
-        content: "On the drop-down menu, click on create Event to create a new event",
+        content: "On the drop-down menu, click on Events then New Event to create a new event.",
         path: "/index.html"
     },
     {
-        element: "#bottom",
+        element: "#top2",
         title: "Part 2: Fill out the event details on the first page",
-        content: "Begin by filling out everything with an asterisk. You could make changes to your event details later",
+        content: "Begin by filling out everything with an asterisk. You could make changes to your event details later.",
         path: "/page2.html"
     },
     {
-        element: "#top",
+        element: "#top3",
         title: "Part 3: Add Event Location",
-        content: "Finally, add the location of the event. Click save & done. Your event is now on the records and you could now add participants",
+        content: "Finally, add the location of the event. Click save & done. Your event is now on the records and you could now add participants.",
         path: "/page3.html"
     },
     {
-        element: "#top",
+        element: "#top4",
         title: "Part 4: Register Participant to Event",
-        content: "Click on Events & Register Event Participants. Search for contact information and add their attendance to new event created. Click save & next to add the next participant",
+        content: "Click on Events & Register Event Participants. Search for contact information and add their attendance to new event created. Click save & next to add the next participant.",
         path: "/page4.html"
     },
     {
-        element: "#top",
+        element: "#top5",
         title: "Part 5: Event is now live!",
         content: "Click Event then dashboard to view you new event listing with all the participants.",
         path: "/page5.html"
